44a3419095dfe15b017874c29d2c14250dd0c524
Le Quang Lam <work.lamsun@gmail.com>
2024-12-25 12:49:30 +0700

    feat: use pug template engines

--- 03_Dynamic_Content_And_Templates/Code_Example/routes/shop.js
+++ 03_Dynamic_Content_And_Templates/Code_Example/routes/shop.js
@@ -8,8 +8,8 @@ const adminData = require("./admin");
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
-  console.log("shop.js", adminData.products);
-  res.render("shop");
+  const products = adminData.products;
+  res.render("shop", { products, pageTitle: "Shop", path: "/" });
 });
 
 module.exports = router;
